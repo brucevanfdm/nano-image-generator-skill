e0ee643fdaecd177ca167a339f5260c4105cb6dd
Bruce Van <brucevanfdm@gmail.com>
2026-01-14 13:46:30 +0800

    Update generate_image.py

--- scripts/generate_image.py
+++ scripts/generate_image.py
@@ -4,6 +4,7 @@ Nano Image Generator - Generate images using Google's Gemini 3 Pro Preview API.
 
 [modify]
 [modify2026年1月14日11:51:56]
+[modify2026年1月14日13:46:19]
 
 
 Requires: Set your GEMINI_API_KEY in the get_api_key() function below.
